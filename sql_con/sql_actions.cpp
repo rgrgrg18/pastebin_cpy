8668710a79c21ad39ad6997194436997470ef3fe
IzKirill <izmailovkirill880@gmail.com>
2024-02-12 17:30:01 +0300

    added 2 new prepared functions: nubmer users pastes and output all pastesof user to file

--- sql_con/sql_actions.cpp
+++ sql_con/sql_actions.cpp
@@ -26,6 +26,7 @@ void sql_actions::prepare_add_paste (pqxx::connection_base& conn) {
 		"add_paste",
 		"INSERT INTO pastes (hash, amazon_link, user_id) \
 		VALUES ($1, $2, (SELECT id FROM users WHERE login = $3))");
+	
 }
 
 unsigned long long sql_actions::execute_get_sequence_for_hash (pqxx::transaction_base& txn) {
@@ -33,22 +34,50 @@ unsigned long long sql_actions::execute_get_sequence_for_hash (pqxx::transaction
 	return txn.exec_prepared("get_sequence_for_hash")[0][0].as(sequence);
 }
 
-std::string sql_actions::execute_get_amazon_link (pqxx::transaction_base& txn) {
-	return (txn.exec_prepared("get_amazon_link"))[0][0].c_str();
+std::string sql_actions::execute_get_amazon_link (pqxx::transaction_base& txn, const std::string& hash_link) {
+	return (txn.exec_prepared("get_amazon_link", hash_link))[0][0].c_str();
+}
+
+void sql_actions::prepare_check_login (pqxx::connection_base& conn) {
+	conn.prepare(
+		"check_login",
+		"SELECT id FROM users WHERE login = $1");
 }
 
 void sql_actions::execute_add_user (pqxx::transaction_base& txn, const std::string& login) {
-	txn.exec_prepared("add_user", login);
+	if (txn.exec_prepared("check_login", login).size() == 0)
+		txn.exec_prepared0("add_user", login);
 }
 
 void sql_actions::execute_add_paste (pqxx::transaction_base& txn, 
 						const std::string& login, 
 						const std::string& amazon_link, 
 						const std::string& hash_link) {
-	txn.exec_prepared("add_paste", hash_link, amazon_link, login);
+	txn.exec_prepared0("add_paste", hash_link, amazon_link, login);
 }
 
-void sql_actions::print_sql_tables(pqxx::work& txn) {
+void sql_actions::prepare_number_pastes (pqxx::connection_base& conn) {
+	conn.prepare(
+		"number_pastes",
+		"SELECT COUNT(*) FROM pastes WHERE user_id = (SELECT id FROM users WHERE login = $1)");
+}
+
+int sql_actions::execute_number_pastes (pqxx::transaction_base& txn, const std::string& login) {
+	int count;
+	return txn.exec_prepared("number_pastes", login)[0][0].as(count);
+}
+
+void sql_actions::prepare_all_pastes (pqxx::connection_base& conn) {
+	conn.prepare(
+		"all_pastes",
+		"COPY (SELECT * FROM pastes WHERE user_id = (SELECT id FROM users WHERE login = 'kirill')) TO '/var/lib/postgresql/test1.txt'");
+}
+
+void sql_actions::execute_all_pastes (pqxx::transaction_base& txn, const std::string& login) {
+	txn.exec_prepared("all_pastes");
+}		
+
+void sql_actions::print_sql_tables (pqxx::work& txn) {
 	pqxx::result res(txn.exec("SELECT * FROM users"));
 
 	if (!res.empty()) {
--- sql_con/sql_actions.h
+++ sql_con/sql_actions.h
@@ -3,6 +3,7 @@
 
 #include <pqxx/pqxx>
 
+
 class sql_actions {
 public:
 	static void print_sql_tables(pqxx::work& txn);
@@ -11,12 +12,17 @@ public:
 	static void prepare_add_paste (pqxx::connection_base& conn);
 	static void prepare_get_amazon_link (pqxx::connection_base& conn);
 	static void prepare_get_sequence_for_hash (pqxx::connection_base& conn);
+	static void prepare_check_login (pqxx::connection_base& conn);
+	static void prepare_number_pastes (pqxx::connection_base& conn);
+	static void prepare_all_pastes (pqxx::connection_base& conn);
+public:
 	static void execute_add_user (pqxx::transaction_base& txn, const std::string& login);
 	static void execute_add_paste (pqxx::transaction_base& txn, const std::string& login, 
 								   const std::string& amazon_link, const std::string& hash_link);
-	static std::string execute_get_amazon_link (pqxx::transaction_base& txn);
+	static std::string execute_get_amazon_link (pqxx::transaction_base& txn, const std::string& hash_link);
 	static unsigned long long execute_get_sequence_for_hash (pqxx::transaction_base& txn);
+	static int execute_number_pastes (pqxx::transaction_base& txn, const std::string& login);
+	static void execute_all_pastes (pqxx::transaction_base& txn, const std::string& login);
 };
-
 
 #endif
