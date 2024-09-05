563a07c373dd230ccbb7188df601263f79497cde
Egor Eroshkin <eroshkin.ea@phystech.edu>
2024-09-05 09:43:20 +0300

    weak attribute was hidden under define

--- src/config.hpp
+++ src/config.hpp
@@ -2,18 +2,27 @@
 
 #include <string>
 
+// Define a macro to hide the weak attribute
+// Use this if more than one file depends on it to avoid linker errors
+#ifdef USE_WEAK
+    #define WEAK_ATTR __attribute__((weak))
+#else
+    #define WEAK_ATTR
+#endif
+
+
 namespace Config {
 
-    std::string __attribute__((weak)) ListenPort = "0.0.0.0:9999";
+    std::string WEAK_ATTR ListenPort = "0.0.0.0:9999";
 
-    std::string __attribute__((weak)) Files_directory = "/usr/src/app/build/";
+    std::string WEAK_ATTR Files_directory = "/usr/src/app/build/";
 
-    std::string __attribute__((weak)) Bucket_name = "";
+    std::string WEAK_ATTR Bucket_name = "";
 
-    std::string __attribute__((weak)) Conn = "postgresql://root:root@postgres:5432/pastebin";
+    std::string WEAK_ATTR Conn = "postgresql://root:root@postgres:5432/pastebin";
 
-    std::string __attribute__((weak)) Endpoint = "";
+    std::string WEAK_ATTR Endpoint = "";
 
-    std::string __attribute__((weak)) Redis_conn = "tcp://redis:6379";
+    std::string WEAK_ATTR Redis_conn = "tcp://redis:6379";
 
-};
+} // namespace Config
