c4b04248413034afb956e44e28c5132f3d177db2
Egor Eroshkin <eroshkin.ea@phystech.edu>
2024-09-05 10:00:36 +0300

    all attributes was hidden using define

--- src/server/methods/settings/redis.hpp
+++ src/server/methods/settings/redis.hpp
@@ -1,7 +1,15 @@
 #pragma once
 
+// Define a macro to hide the weak attribute
+// Use this if more than one file depends on it to avoid linker errors
+#ifdef USE_WEAK
+    #define WEAK_ATTR __attribute__((weak))
+#else
+    #define WEAK_ATTR
+#endif
+
 namespace redisSettins {
 
-    int __attribute__((weak)) lifeTime = 600; // lifeTime in seconds
+    int WEAK_ATTR lifeTime = 600; // lifeTime in seconds
 
 };
