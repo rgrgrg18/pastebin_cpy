86ed14a7a84b9826abbe61dad9ffd6dbfe59d8dc
rgrgrg18 <eroshkin.ea@phystech.edu>
2024-12-08 17:48:29 +0300

    common logic in shared class

--- src/reader/utility/random_paste_generator.hpp
+++ src/reader/utility/random_paste_generator.hpp
@@ -7,7 +7,7 @@ namespace utility {
 namespace testing {
 
 pastebin::Paste GeneratePaste(uint64_t seed);
-pastebin::PasteText GeneratePasteText(uint64_t seed);
+pastebin::PasteText GeneratePasteText(uint64_t seed, uint64_t min_length = 0, uint64_t max_length = 1'000'000);
 pastebin::PasteMetadata GeneratePasteMetadata(uint64_t seed);
 std::string GeneratePublicKey(uint64_t seed);
 
